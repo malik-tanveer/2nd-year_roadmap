a089d5e7f419f5d7823667e9143a4d76746f3923
tanveer-developer123 <tanveerlinux4@gmail.com>
2025-10-09 20:28:18 +0500

    Week 2 Day 1 Completd

--- Week 2/Day 1/Class.java
+++ Week 2/Day 1/Class.java
@@ -1,6 +1,16 @@
+class A {
+    void showA() {
+        System.out.println("This is a Class A");
+    }
+}
+
+
 public class Class {
     public static void main(String[] args) {
         System.out.println("Class in jva");
+
+        A objA = new A();
+        objA.showA();
     }
-    
+
 }
--- Week 2/Day 1/Obj.java
+++ Week 2/Day 1/Obj.java
@@ -1,6 +1,19 @@
+class car {
+    String brand;
+    int year;
+    String color;
+    void display(){
+        System.out.println(brand + " " + year + " " + color);
+    }
+}
+
 public class Obj {
     public static void main(String[] args) {
-        System.out.println("Objects in Java");
+        car c1 = new car();
+        c1.brand = "Honda";
+        c1.color = "Black";
+        c1.year = 2023;
+        c1.display();
     }
     
 }
